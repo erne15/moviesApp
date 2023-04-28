244d2997691b2719f88a9f1e565f8903d2229517
Ernest Mwiti <Ernest.Mwiti@CRAFTSILICON.LOCAL>
2023-04-28 11:57:17 +0300

    mvc app first commit

---
+++ moviesApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moviesApp.Models;
+using System.Diagnostics;
+
+namespace moviesApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ moviesApp/Data/moviesAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using moviesApp.Models;
+
+namespace moviesApp.Data
+{
+    public class moviesAppContext : DbContext
+    {
+        public moviesAppContext (DbContextOptions<moviesAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<moviesApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
